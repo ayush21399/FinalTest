e9302d4197d1af9e598327621cabf8ab1aef8ba3
apate666 <300987477@cencol.ca>
2018-08-15 11:29:05 -0400

    first part aadding done(major part still need to do work)

--- public/Scripts/objects/contact.js
+++ public/Scripts/objects/contact.js
@@ -1,8 +1,22 @@
 let objects;
 (function(){
     class Contact {
-        // your class definition goes here
+        // your class definition goes here   
     }
+
+function mine(first, last, cont, em) {
+        this.firstName = first;
+        this.lastName = last;
+        this.contact = cont;
+        this.email = em;
+        this.name = function() {
+            return this.firstName + " " + this.lastName
+        };
+    }
+
+    var myname = new mine("ayush", "patel", 1111111111, "ayush21031999@gmail.com");
+// Display full name
+document.getElementsByClassName("display-6").innerHTML = "My name is " + myname.name(); 
 
     objects.Contact = Contact;
 
